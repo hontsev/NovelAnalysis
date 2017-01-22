dd984925bb4fbd660ebf6d590fa4b7e7ce726e88
hontsev <hontsev@gmail.com>
2017-01-23 00:04:03 +0800

    233

---
+++ NovelAnalysis/AnalysisTools/SentenceAnalysis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NovelAnalysis.AnalysisTools
+{
+    class SentenceAnalysis
+    {
+        private DataController dc;
+
+        public SentenceAnalysis(DataController data)
+        {
+            dc = data;
+        }
+
+
+    }
+}
---
+++ NovelAnalysis/AnalysisTools/WordCutPB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NovelAnalysis.AnalysisTools
+{
+    public class WordCutPB
+    {
+        private DataController dc;
+
+
+        public WordCutPB(DataController data)
+        {
+            dc = data;
+        }
+
+
+    }
+}
